5430a9cd695aa944a93bc34170839dca8d11fad5
Tom <tommapcoder@gmail.com>
2024-12-10 14:04:12 +0700

    parameterized & repeated test

--- src/test/java/sample/CalculatorTest.java
+++ src/test/java/sample/CalculatorTest.java
@@ -4,14 +4,24 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.tommap.sample.Calculator;
 
+import java.util.stream.Stream;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.fail;
 
 class CalculatorTest {
     Calculator calculator;
@@ -27,12 +37,34 @@ class CalculatorTest {
         calculator = new Calculator(); //clean state
     }
 
-
     //naming convention - test<MethodUnderTest>_<Scenario>_<ExpectedBehavior>
     @Test
     @DisplayName("Test 10/2=5")
     void testIntegerDivision_WhenTenIsDividedByTwo_ShouldReturnFive() {
         System.out.println("Executing Test 10/2=5...");
+
+        //arrange
+        int dividend = 10;
+        int divisor = 2;
+        int expectedResult = 5;
+
+        //act
+        int actualResult = calculator.integerDivision(dividend, divisor);
+
+        //assert
+        assertEquals(expectedResult, actualResult,
+                () -> dividend + "/" + divisor + " did not produce " + expectedResult //using lambda to defer string concatenation - only computed if test fails
+        );
+    }
+
+    @RepeatedTest(value = 3, name = "{displayName} - repetition {currentRepetition} of {totalRepetitions}") //make sure behavior is consistent even executing multiple times
+    @DisplayName("Test 10/2=5 with @RepeatedTest")
+    void testIntegerDivision_WhenUsingRepeatedTestAnnotation(
+            RepetitionInfo repetitionInfo,
+            TestInfo testInfo
+    ) {
+        System.out.println("Running Test: " + testInfo.getTestMethod().get().getName());
+        System.out.println("Repetition #" + repetitionInfo.getCurrentRepetition() + " of " + repetitionInfo.getTotalRepetitions());
 
         //arrange
         int dividend = 10;
@@ -68,15 +100,28 @@ class CalculatorTest {
         assertEquals(expectedExceptionMessage, actualException.getMessage(), "Unexpected exception message...");
     }
 
-    @Test
-    @DisplayName("Test 10-5=5")
-    void testIntegerSubtraction_WhenTenIsSubtractedByFive_ShouldReturnFive() {
-        System.out.println("Executing Test 10-5=5...");
+    @ParameterizedTest
+    @ValueSource(strings = {"Tom", "Jerry", "Khanh"}) //only one argument
+    @DisplayName("Test value source annotation")
+    void valueSourceAnnotation(String name) {
+        System.out.println("Executing Test " + name + "...");
 
         //arrange
-        int minuend = 10;
-        int subtrahend = 5;
-        int expectedResult = 5;
+        //act
+
+        //assert
+        assertNotNull(name);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/integerSubtraction.csv")
+    @DisplayName("Test integer subtraction using @CsvFileSource")
+    void testIntegerSubtraction_WhenUsingCsvFileSourceAnnotation(
+            int minuend, int subtrahend, int expectedResult
+    ) {
+        System.out.println("Executing Test " + minuend + "-" + subtrahend + "=" + expectedResult + "...");
+
+        //arrange
 
         //act
         int actualResult = calculator.integerSubtraction(minuend, subtrahend);
@@ -84,6 +129,56 @@ class CalculatorTest {
         //assert
         assertEquals(expectedResult, actualResult,
                 () -> minuend + "-" + subtrahend + " did not produce " + expectedResult
+        );
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "10, 5, 5",
+            "20, 5, 15",
+            "8, 5, 3"
+    })
+    @DisplayName("Test integer subtraction using @CsvSource")
+    void testIntegerSubtraction_WhenUsingCsvSourceAnnotation(
+            int minuend, int subtrahend, int expectedResult
+    ) {
+        System.out.println("Executing Test " + minuend + "-" + subtrahend + "=" + expectedResult + "...");
+
+        //arrange
+
+        //act
+        int actualResult = calculator.integerSubtraction(minuend, subtrahend);
+
+        //assert
+        assertEquals(expectedResult, actualResult,
+                () -> minuend + "-" + subtrahend + " did not produce " + expectedResult
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("integerSubtractionInputParameters")
+    @DisplayName("Test integer subtraction")
+    void testIntegerSubtraction_WhenTenIsSubtractedByFive_ShouldReturnFive(
+            int minuend, int subtrahend, int expectedResult
+    ) {
+        System.out.println("Executing Test " + minuend + "-" + subtrahend + "=" + expectedResult + "...");
+
+        //arrange
+
+        //act
+        int actualResult = calculator.integerSubtraction(minuend, subtrahend);
+
+        //assert
+        assertEquals(expectedResult, actualResult,
+                () -> minuend + "-" + subtrahend + " did not produce " + expectedResult
+        );
+    }
+
+    private static Stream<Arguments> integerSubtractionInputParameters() {
+        return Stream.of(
+                Arguments.of(10, 5, 5),
+                Arguments.of(20, 5, 15),
+                Arguments.of(8, 5, 3)
         );
     }
 
