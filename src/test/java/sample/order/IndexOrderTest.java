a3d52b6f01deb04d1294474e3d0f7108027f20d5
Tom <tommapcoder@gmail.com>
2024-12-10 15:44:37 +0700

    method order test

---
+++ src/test/java/sample/order/IndexOrderTest.java
@@ -0,0 +1,37 @@
+package sample.order;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/*
+    - JUnit by default runs tests using a deterministic but unpredictable order
+    - reference: https://www.baeldung.com/junit-5-test-order
+ */
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class IndexOrderTest {
+    @Test
+    @Order(1)
+    void firstTest() {
+        System.out.println("Executing first test...");
+    }
+
+    @Test
+    @Order(2)
+    void secondTest() {
+        System.out.println("Executing second test...");
+    }
+
+    @Test
+    @Order(3)
+    void thirdTest() {
+        System.out.println("Executing third test...");
+    }
+
+    @Test
+    @Order(4)
+    void fourthTest() {
+        System.out.println("Executing fourth test...");
+    }
+}
---
+++ src/test/java/sample/order/NameOrderTest.java
@@ -0,0 +1,32 @@
+package sample.order;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+
+/*
+    - JUnit by default runs tests using a deterministic but unpredictable order
+    - reference: https://www.baeldung.com/junit-5-test-order
+ */
+@TestMethodOrder(MethodOrderer.MethodName.class)
+class NameOrderTest {
+    @Test
+    void firstTest() {
+        System.out.println("Executing first test...");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("Executing second test...");
+    }
+
+    @Test
+    void thirdTest() {
+        System.out.println("Executing third test...");
+    }
+
+    @Test
+    void fourthTest() {
+        System.out.println("Executing fourth test...");
+    }
+}
